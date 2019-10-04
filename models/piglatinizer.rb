5d3b6c2b08cf4755a96ae9ef550c3444f9957d50
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-04 19:06:30 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,5 @@
 class PigLatinizer 
-  attr_reader :input
-  
-   def piglatinize(input_str)
+  def piglatinize(input_str)
     input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
   end
 
